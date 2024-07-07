f2a13cfd2235ec4039e5814fab434c252133b307
InternBabu <shhbzsddq1@gmail.com>
2024-07-07 14:00:35 +0530

    changes
    changes

--- src/components/Weatherbox.js
+++ src/components/Weatherbox.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
+import axios from 'axios'
 
 import "./weather.css";
 
@@ -58,61 +59,86 @@ export default function Weatherbox() {
 
   let api_key = "73c969463cb3b13474ae65c858ca443b";
 
-  const [wimg,setwimg]=useState(mist);
+  const [weather, setWeather] = useState(null);
+  const [wimg, setWimg] = useState(mist);
+  const [searchQuery, setSearchQuery] = useState('');
 
-  const search = async () => {
-    const elem = document.getElementsByClassName("input");
-    if (elem[0].value === "") {
-      return 0;
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=New Delhi&units=Metric&appid=${api_key}`);
+        setWeather(response.data);
+        setWeatherImage(response.data.weather[0].icon);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
+
+    fetchWeather();
+  }, []);
+
+  const search = async (event) => {
+    if (event) event.preventDefault();
+    if (searchQuery === "") {
+      return;
     }
     let url =
-      `https://api.openweathermap.org/data/2.5/weather?q=${elem[0].value}&units=Metric&appid=${api_key}`;
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&units=Metric&appid=${api_key}`;
        let response = await fetch(url);
-       let data = await response.json()
-       const humidity = document.getElementsByClassName("Humidity");
-       const wind = document.getElementsByClassName("Wind");
-       const temp = document.getElementsByClassName("temp");
-       const loc = document.getElementsByClassName("City");
-
-       humidity[0].innerHTML = data.main.humidity+"% Humidity";
-       wind[0].innerHTML = data.wind.speed+"km/h Wind Speed";
-       temp[0].innerHTML = data.main.temp+"°c";
-       loc[0].innerHTML = data.name;
-       if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n"){
-          setwimg(clear)
-       }
-       else if(data.weather[0].icon==="02d" || data.weather[0].icon==="02n"){
-        setwimg(cloud)
-     }
-       else if(data.weather[0].icon==="03d" || data.weather[0].icon==="03n"){
-        setwimg(drizzle)
-     }
-       else if(data.weather[0].icon==="04d" || data.weather[0].icon==="04n"){
-        setwimg(drizzle)
-     }
-       else if(data.weather[0].icon==="09d" || data.weather[0].icon==="09n"){
-        setwimg(rain)
-     }
-       else if(data.weather[0].icon==="010d" || data.weather[0].icon==="010n"){
-        setwimg(rain)
-     }
-       else if(data.weather[0].icon==="011d" || data.weather[0].icon==="011n"){
-        setwimg(strome)
-     }
-       else if(data.weather[0].icon==="013d" || data.weather[0].icon==="013n"){
-        setwimg(snow)
-     }
-     else{
-      setwimg(mist)
-     }
-     
+       let data = await response.json();
+       setWeather(data);
+       setWeatherImage(data.weather[0].icon); 
   };
+
+       const setWeatherImage = (icon) => {
+        switch (icon) {
+          case "01d":
+          case "01n":
+            setWimg(clear);
+            break;
+          case "02d":
+          case "02n":
+            setWimg(cloud);
+            break;
+          case "03d":
+          case "03n":
+            setWimg(drizzle);
+            break;
+          case "04d":
+          case "04n":
+            setWimg(drizzle);
+            break;
+          case "09d":
+          case "09n":
+            setWimg(rain);
+            break;
+          case "10d":
+          case "10n":
+            setWimg(rain);
+            break;
+          case "11d":
+          case "11n":
+            setWimg(strome);
+            break;
+          case "13d":
+          case "13n":
+            setWimg(snow);
+            break;
+          default:
+            setWimg(mist);
+            break;
+        }
+      };
+     
   return (
     <>
       <div className="Weatherbox">
         <div className="mainbox">
-          <div className="searchbox">
-            <input class="input" name="text" placeholder="Search..." type="search"/>
+          <form className="searchbox" onSubmit={search}>
+            <input class="input" name="text" placeholder="Search..." type="search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            />
             <div
               className="search"
               onClick={() => {
@@ -130,34 +156,40 @@ export default function Weatherbox() {
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
               </svg>
             </div>
-          </div>
+          </form>
           <div className="main">
             <div className="left">
-              <div className="">
-                <p className="temp">30°C</p>
-              </div>
-              <div className="">
-                <h3 className="City">New Delhi</h3>
-              </div>
-              <div className="">
-                <i><p className="Humidity">64% Humidity</p></i>
-              </div>
-              <div>
-                <img src={humidity} alt="Humidity" />
-              </div>
-              <div className="">
-                <i><p className="Wind">18 km/h Wind Speed</p></i>
-              </div>
-              <div>
+            {weather ? (
+              <>
+                <div>
+                  <p className="temp">{weather.main.temp}°c</p>
+                </div>
+                <div>
+                  <h3 className="City">{weather.name}</h3>
+                </div>
+                <div>
+                  <i><p className="Humidity">{weather.main.humidity}% Humidity</p></i>
+                </div>
+                <div>
+                  <img src={humidity} alt="wind" />
+                </div>
+                <div>
+                  <i><p className="Wind">{weather.wind.speed} km/h Wind Speed</p></i>
+                </div>
+                <div>
                 <img src={wind} alt="wind" />
-              </div>
+                </div>
+              </>
+            ) : (
+              <div></div>
+            )}
             </div>
             <div className="right">
               <div className="weatherimg">
                 <img src={wimg} alt="weather" />
               </div>
                 <div className="date">{dateG(new Date())}</div>
-                <div className="time">{cTime}</div>
+                {/* <div className="time">{cTime}</div> */}
               </div>
           </div>
         </div>
